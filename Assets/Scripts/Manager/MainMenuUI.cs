e5551e8be150cc7bf4ae868965e0104458bbb58b
Jichao-tj <75312573+Jichao-tj@users.noreply.github.com>
2025-10-24 00:15:13 +0200

    Start game from main menu

--- Assets/Scripts/Manager/MainMenuUI.cs
+++ Assets/Scripts/Manager/MainMenuUI.cs
@@ -13,6 +13,11 @@ public class MainMenuUI : MonoBehaviour
 
     public void StartGame()
     {
+        SceneManager.LoadScene("Clinic_Day");
+    }
+
+    public void StartNightGame()
+    {
         SceneManager.LoadScene("Game");
     }
 
